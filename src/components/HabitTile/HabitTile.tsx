438e2ed1e88f83287fd178fe1046beecdc36cc72
hjg <harrisongerstenlauer@hgmbp.myfiosgateway.com>
2023-03-16 14:41:17 -0400

    implement axios to fetch data, pass data to habit card props for rendering

--- src/components/HabitTile/HabitTile.tsx
+++ src/components/HabitTile/HabitTile.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { TileContainer } from './HabitTile.styled'
 import headshot from '../../assets/headshot.jpg'
+import { HabitData } from "../../pages/Home/Home";
 
-export default function HabitTile() {
+
+export default function HabitTile(props: HabitData) {
+  const { habit_name, icon, start_date, description } = props;
   return (
     <TileContainer>
-        <div>
-          <img src={headshot} />
-        </div>
-        <h1>cool tile</h1>
+      <div>
+        <img src={icon} />
+        <h1>{habit_name}</h1>
+      </div>
     </TileContainer>
-  )
+  );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { HomeContainer } from "./Home.styled";
 import HabitTile from "../../components/HabitTile/HabitTile";
+import axios from "axios";
+
+export interface HabitData {
+  habit_name: string;
+  icon: string;
+  start_date: string;
+  description: string;
+}
 
 export default function Home() {
+  const [data, setData] = useState<HabitData[]>([]);
+  // const fetchData = async () => {
+  //   const newData = await fetch("dummyData.json", {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Accept: "application/json",
+  //     },
+  //   });
+  //   const jsonData = newData.json();
+  // };
+  useEffect(() => {
+    axios.get("dummyData.json").then((res) => {
+      setData(res.data);
+    });
+
+  }, []);
+
   return (
     <HomeContainer>
-      <HabitTile />
-      <HabitTile />
-      <HabitTile />
-      <HabitTile />
+      {data.map((habit) => {
+        return (
+          <HabitTile {...habit} />
+        )
+      })}
     </HomeContainer>
   );
 }
