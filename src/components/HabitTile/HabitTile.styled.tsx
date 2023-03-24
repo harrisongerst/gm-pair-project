65148bf3326f943eaf367ebac057326e34a7d23d
hjg <harrisongerstenlauer@hgMBP.local>
2023-03-24 10:03:22 -0400

    remove comments

--- src/components/HabitTile/HabitTile.styled.tsx
+++ src/components/HabitTile/HabitTile.styled.tsx
@@ -8,8 +8,6 @@ interface HabitProps {
 
 
 export const TileContainer = styled.div<HabitProps>`
-  /* height: 30vh; */
-  /* flex-grow: 1; */
   list-style-type: none;
   margin: 20px;
   align-items: center;
@@ -31,16 +29,6 @@ export const TileContainer = styled.div<HabitProps>`
         return "white"
       }
     }
-    // if(props.isBadHabit) {
-    //   return "green"
-    // } else if(!props.isBadHabit){
-    //   return "white"
-    // } else if(!props.isBadHabit && (props.maxAmount === props.count)){
-    //   return "green"
-    // } else if(props.isBadHabit && props.maxAmount === props.count){
-    //   return "red";
-    // }
-
   }};
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0px 40px rgba(0, 0, 0, 0.08);
   border-radius: 5px;
